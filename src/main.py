4197e054a78f5fd40ddc8098ab663f6828bc3e88
rahman-io <rahman.portfolio.collection@gmail.com>
2025-08-01 08:22:48 +0800

    feat: Set up initial project structure with src layout

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import random
+
+def calculator():
+    print("\n--- Calculator Activated ---")
+    pass
+
+def guessing_game():
+    print("\n--- Guessing Game Activated ---")
+    pass
+
+def main():
+    print("Welcome to my first multi-program project!")
+
+if __name__ == "__main__":(
+    main())
